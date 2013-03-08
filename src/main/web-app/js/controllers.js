de90e884b073a0d0f56247e954d75ab05775be1f
cmarks <cmarks@cmwareonline.com>
2013-03-07 22:23:46 -0700

    Got a timer on the javascript side of things working to poll the server for a new value.

--- src/main/web-app/js/controllers.js
+++ src/main/web-app/js/controllers.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function ArchaiusCtrl($scope, $http) {
-    $http.get("/ws/archaius").success(function(data){
-        $scope.data = data;
-    });
+    $scope.checkCount = 0;
+    $scope.archTimer = setInterval(function(){
+        $http.get("/ws/archaius").success(function(data){
+            $scope.data = data;
+        });
+        $scope.checkCount++;
+    }, 1000);
+
+    $scope.stopCheck = function(){
+        clearInterval($scope.archTimer);
+    };
 }
 //ArchaiusCtrl.$inject = ['$scope', '$http'];
