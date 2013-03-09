680a71e34e146d8394a0f62464b458672a42bef6
cmarks <cmarks@cmwareonline.com>
2013-03-08 21:59:02 -0700

    Got the servo library installed and configured to provide a counter via JMX.

--- src/main/java/com/cmware/ws/ArchaiusResource.java
+++ src/main/java/com/cmware/ws/ArchaiusResource.java
@@ -2,6 +2,9 @@ package com.cmware.ws;
 
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
+import com.netflix.servo.annotations.DataSourceType;
+import com.netflix.servo.annotations.Monitor;
+import com.netflix.servo.monitor.Monitors;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
@@ -15,6 +18,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.ws.WebServiceException;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Resource dedicated to provide REST based access to a set of properties managed and populated by Archaius.
@@ -25,16 +29,21 @@ import javax.xml.ws.WebServiceException;
 public class ArchaiusResource {
 
     private static final Logger log = LoggerFactory.getLogger(ArchaiusResource.class);
-
     private DynamicStringProperty sampleProperty;
+
+    @Monitor(name="requestCount", type= DataSourceType.COUNTER)
+    private AtomicLong requestCounter = new AtomicLong(0);
 
     public ArchaiusResource() {
         sampleProperty = DynamicPropertyFactory.getInstance().getStringProperty("sampleProperty", "Hello world!");
+        Monitors.registerObject(this);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JSONObject getProperties() {
+
+        requestCounter.incrementAndGet();
         JSONObject jsonValue = new JSONObject();
         try {
             jsonValue.put("sampleProperty", sampleProperty.get());
